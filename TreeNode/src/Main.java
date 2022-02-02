d53d8e5846cbf68c3150639c472999fa1935adf8
Gizmo10 <sw.ndzala@gmail.com>
2022-02-01 22:41:22 -0900

    Added tests for listOfDepths v1

--- TreeNode/src/Main.java
+++ TreeNode/src/Main.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int [] values = {1,3,5,7,9,10,13,19};
+        int[] values = {1, 3, 5, 7, 9, 10, 13, 19};
         TreeNode root = new TreeNode();
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
         root.minimalTree(values);
-        TreeNode.preorderTraversal(root);
+        //TreeNode.preorderTraversal(root);
+
+        list = TreeNode.listOfDepths(root);
+
+        for (Integer i : list.get(0)) {
+
+            System.out.println(i);
+        }
     }
 }
