7ae99b31203f1f7897d0a6d11b8fe9c007f6095e
Gizmo10 <sw.ndzala@gmail.com>
2022-02-04 20:00:22 -0900

    Tests passed

---
+++ TwoElementsSum/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String [] args){
+
+        int [] arr = {1,2,3,4,5,6,7};
+        int sum = 9;
+
+        System.out.println(Arrays.toString(TwoElementsSum.twoElements(arr,sum)));
+    }
+}
