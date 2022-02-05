2acbf9ac433f3a40c5ebfda092be9170c93c6b06
Gizmo10 <sw.ndzala@gmail.com>
2022-02-04 19:51:33 -0900

    Version 1 of method done

---
+++ TwoElementsSum/src/TwoElementsSum.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class TwoElementsSum {
+
+    public static int[] twoElements(int[] elements, int sum){
+
+        int [] result = new int[2];
+        HashMap<Integer,Integer> pairsAddingToSum = new HashMap<>();
+
+        for(int i = 0;i < elements.length;i++){
+
+            if(!pairsAddingToSum.containsKey(elements[i])){
+
+                pairsAddingToSum.put(elements[i], sum - elements[i]);
+            }
+        }
+
+        for(int j = 0;j < elements.length;j++){
+
+            if(pairsAddingToSum.containsKey(pairsAddingToSum.get(elements[j]))){
+
+                result[0] = elements[j];
+                result[1] = pairsAddingToSum.get(elements[j]);
+            }
+        }
+
+        return result;
+    }
+
+}
